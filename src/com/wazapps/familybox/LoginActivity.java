e7f3eff0d4e58fae35110e695818cf5993900527
NITZAN WERBER <nitzan.werber@gmail.com>
2014-10-03 15:29:54 +0300

    first commit

---
+++ src/com/wazapps/familybox/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.wazapps.familybox;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login_screen);
+	}
+}
