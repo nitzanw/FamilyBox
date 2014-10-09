9dec8331d240cdc4c973eff105ef2e598227ab91
galzo <galzohar@gmail.com>
2014-10-09 16:09:38 +0300

    added more data structures for support in future sticky headers listview (families list)

--- src/com/wazapps/familybox/familyTree/FamiliesListItem.java
+++ src/com/wazapps/familybox/familyTree/FamiliesListItem.java
@@ -1,5 +1,49 @@
 package com.wazapps.familybox.familyTree;
 
-public class FamiliesListItem {
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class FamiliesListItem implements Parcelable {
+	private String familyId, familyName;
+	
+	public static final Parcelable.Creator<FamiliesListItem> CREATOR = 
+			new Parcelable.Creator<FamiliesListItem>() {
+		
+		public FamiliesListItem createFromParcel(Parcel source) {
+			return new FamiliesListItem(source);
+		}
+
+		public FamiliesListItem[] newArray(int size) {
+			return new FamiliesListItem[size];
+		}
+	};
+	
+	public FamiliesListItem(String familyId, String familyName) {
+		this.familyId = familyId;
+		this.familyName = familyName;
+	}
+	
+	public FamiliesListItem(Parcel data) {
+		this.familyId = data.readString();
+		this.familyName = data.readString();
+	}
+	
+	public String getFamilyId() {
+		return familyId;
+	}
+
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.familyId);
+		dest.writeString(this.familyName);
+	}
 }
