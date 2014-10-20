1bf364e3dbb9de0b078bb13f3949f8fc80217223
NITZAN WERBER <nitzan.werber@gmail.com>
2014-10-21 01:24:48 +0300

    fix search bug

--- src/com/wazapps/familybox/familyTree/FamiliesListFragment.java
+++ src/com/wazapps/familybox/familyTree/FamiliesListFragment.java
@@ -1,5 +1,15 @@
 package com.wazapps.familybox.familyTree;
 
+import com.wazapps.familybox.familyProfiles.FamilyProfileScreenActivity;
+import com.wazapps.familybox.photos.AlbumItem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
 public class FamiliesListFragment extends BasicFamilyListFragment {
 	public static final String FAMILY_TREE_FRAG = "family tree fragment";
 	FamiliesListAdapter familiesListAdapater;
@@ -11,6 +21,18 @@ public class FamiliesListFragment extends BasicFamilyListFragment {
 				familiesListData);
 		familiesList.setAdapter(familiesListAdapater);
 
+	}
+
+	@Override
+	protected void createOnClickOperation(View v) {
+		Intent familyAlbumsIntent = new Intent(getActivity(),
+				FamilyProfileScreenActivity.class);
+		AlbumItem[] albumList = { null, null, null, null, null, null };
+		Bundle args = new Bundle();
+		args.putParcelableArray("test1", albumList);
+		familyAlbumsIntent.putExtra("test2", args);
+		getActivity().startActivity(familyAlbumsIntent);
+		
 	}
 
 }
--- src/com/wazapps/familybox/photos/SharedAlbumFragment.java
+++ src/com/wazapps/familybox/photos/SharedAlbumFragment.java
@@ -1,6 +1,13 @@
 package com.wazapps.familybox.photos;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
 import com.wazapps.familybox.familyTree.BasicFamilyListFragment;
+import com.wazapps.familybox.familyTree.FamiliesListItem;
 
 public class SharedAlbumFragment extends BasicFamilyListFragment {
 	SharedAlbumsListAdapter sharedAlbumsAdapter;
@@ -13,6 +20,35 @@ public class SharedAlbumFragment extends BasicFamilyListFragment {
 		familiesList.setAdapter(sharedAlbumsAdapter);
 	}
 
+	@Override
+	protected void createOnClickOperation(View v) {
+		FamiliesListItem currFamily = (FamiliesListItem) v.getTag(FAMILT_ITEM);
+		Intent familyAlbumsIntent = new Intent(getActivity(),
+				AlbumGridScreenActivity.class);
+		// generate temp data for album intent
+		// TODO: create real data
+		AlbumItem[] albumList = { null, null, null, null, null, null };
+		String albumName = "Temp Album Name ";
+		PhotoItem[] tempData = { null, null, null, null, null, null, null,
+				null, null, null, null, null, null, null, null, null, null,
+				null };
 
+		for (int i = 0; i < 18; i++) {
+			tempData[i] = new PhotoItem("11.2.201" + i, "www.bla.com",
+					"This is me and my friend Dan " + i);
+		}
+
+		for (int i = 0; i < 6; i++) {
+			albumList[i] = new AlbumItem(String.valueOf(i), tempData, albumName
+					+ i, "December 201" + i);
+		}
+
+		Bundle args = new Bundle();
+		args.putParcelableArray(PhotoGridFragment.ALBUM_ITEM_LIST, albumList);
+		args.putString(FamiliesListItem.FAMILY_NAME, currFamily.getFamilyName());
+		familyAlbumsIntent.putExtra(PhotoGridFragment.ALBUM_ITEM_LIST, args);
+		getActivity().startActivity(familyAlbumsIntent);
+
+	}
 
 }
