d18f1eb4c5d4ae000a88950d8a0d777df4bc83d1
NITZAN WERBER <nitzan.werber@gmail.com>
2014-10-25 18:41:09 +0300

    add progress spinner to login and signup, fix image fade animations

--- src/com/wazapps/familybox/splashAndLogin/EmailSignupFragment.java
+++ src/com/wazapps/familybox/splashAndLogin/EmailSignupFragment.java
@@ -20,26 +20,32 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class EmailSignupFragment extends Fragment 
-implements OnClickListener, OnFocusChangeListener{
+public class EmailSignupFragment extends Fragment implements OnClickListener,
+		OnFocusChangeListener {
 	private View root;
 	private SignupScreenCallback signupCallback = null;
-	private EditText birthday, firstName, lastName, 
-	password, passwordConfirm, gender;
-	
+	private EditText birthday, firstName, lastName, password, passwordConfirm,
+			gender;
+
 	private RoundedImageView uploadImage;
 	private EditText email;
 	private byte[] profilePictureData;
 	private String profilePictureName;
+	private LinearLayout signupProgress;
 
 	public interface SignupScreenCallback {
 		public void openBirthdayInputDialog();
+
 		public void openGenderInputDialog();
+
 		public void openPhonePhotoBrowsing();
+
 		public void signUp(String firstName, String lastName, String email,
-				String birthday, String gender, String password, String passwordConfirm,
-				byte[] profilePictureData, String profilePictureName);
+				String birthday, String gender, String password,
+				String passwordConfirm, byte[] profilePictureData,
+				String profilePictureName);
 	}
 
 	@Override
@@ -47,18 +53,19 @@ implements OnClickListener, OnFocusChangeListener{
 		super.onAttach(activity);
 		try {
 			this.signupCallback = (SignupScreenCallback) getActivity();
-		} 
+		}
 
 		catch (ClassCastException e) {
-			Log.e(getTag(), "the activity does not implement " +
-					"SignupScreenCallback interface");
+			Log.e(getTag(), "the activity does not implement "
+					+ "SignupScreenCallback interface");
 		}
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		root = inflater.inflate(R.layout.fragment_signup_email, container, false);		
+		root = inflater.inflate(R.layout.fragment_signup_email, container,
+				false);
 		initViews();
 		profilePictureData = null;
 		profilePictureName = "";
@@ -66,7 +73,8 @@ implements OnClickListener, OnFocusChangeListener{
 	}
 
 	private void initViews() {
-		this.uploadImage = (RoundedImageView) root.findViewById(R.id.riv_signup_upload_image);
+		this.uploadImage = (RoundedImageView) root
+				.findViewById(R.id.riv_signup_upload_image);
 		this.uploadImage.setOnClickListener(this);
 		this.firstName = (EditText) root.findViewById(R.id.et_signup_name);
 		this.lastName = (EditText) root.findViewById(R.id.et_signup_last);
@@ -78,23 +86,26 @@ implements OnClickListener, OnFocusChangeListener{
 		this.gender.setOnFocusChangeListener(this);
 		this.gender.setOnClickListener(this);
 		this.password = (EditText) root.findViewById(R.id.et_signup_password);
-		this.passwordConfirm = (EditText) root.findViewById(R.id.et_signup_confirm_password);
+		this.passwordConfirm = (EditText) root
+				.findViewById(R.id.et_signup_confirm_password);
+		this.signupProgress = (LinearLayout) root
+				.findViewById(R.id.ll_progress_spinner);
 
 		Button signupButton = (Button) root.findViewById(R.id.button_signup);
 		signupButton.setOnClickListener(this);
 	}
 
 	/**
-	 * Sets a date in the birthday field. used by the wrapping activity 
-	 * to set a date via the BirthdaySignupDialogFragment
+	 * Sets a date in the birthday field. used by the wrapping activity to set a
+	 * date via the BirthdaySignupDialogFragment
 	 */
 	public void setBirthday(String date) {
 		this.birthday.setText(date);
 	}
-	
+
 	/**
-	 * Sets a gender in the gender field. used by the wrapping activity
-	 * to set a date via the GenderSignupDialogFragment
+	 * Sets a gender in the gender field. used by the wrapping activity to set a
+	 * date via the GenderSignupDialogFragment
 	 */
 	public void setGender(String gender) {
 		this.gender.setText(gender);
@@ -109,7 +120,7 @@ implements OnClickListener, OnFocusChangeListener{
 					.getSystemService(Context.INPUT_METHOD_SERVICE);
 			bimm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 			break;
-			
+
 		case R.id.et_signup_gender:
 			signupCallback.openGenderInputDialog();
 			InputMethodManager gimm = (InputMethodManager) getActivity()
@@ -122,24 +133,31 @@ implements OnClickListener, OnFocusChangeListener{
 			break;
 
 		case R.id.button_signup:
-			String firstNameContent, lastNameContent, emailContent, 
-			birthdayContent, genderContent, pwContent, pwConfirmContent;
-			
-			firstNameContent = firstName.getText().toString().trim().toLowerCase();
-			lastNameContent = lastName.getText().toString().trim().toLowerCase();
+			String firstNameContent,
+			lastNameContent,
+			emailContent,
+			birthdayContent,
+			genderContent,
+			pwContent,
+			pwConfirmContent;
+
+			firstNameContent = firstName.getText().toString().trim()
+					.toLowerCase();
+			lastNameContent = lastName.getText().toString().trim()
+					.toLowerCase();
 			emailContent = email.getText().toString().trim().toLowerCase();
 			birthdayContent = birthday.getText().toString().trim();
 			genderContent = gender.getText().toString().trim().toLowerCase();
 			pwContent = password.getText().toString().trim();
 			pwConfirmContent = passwordConfirm.getText().toString().trim();
-			
-			//TODO: set a method for clearing problematic text fields (probably using
-			//exceptions or function return value
-			
-			signupCallback.signUp(firstNameContent, lastNameContent, 
-					emailContent, birthdayContent, genderContent, 
-					pwContent, pwConfirmContent, profilePictureData,
-					profilePictureName);
+
+			// TODO: set a method for clearing problematic text fields (probably
+			// using
+			// exceptions or function return value
+
+			signupCallback.signUp(firstNameContent, lastNameContent,
+					emailContent, birthdayContent, genderContent, pwContent,
+					pwConfirmContent, profilePictureData, profilePictureName);
 			break;
 
 		default:
@@ -158,7 +176,7 @@ implements OnClickListener, OnFocusChangeListener{
 				bimm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 			}
 			break;
-			
+
 		case R.id.et_signup_gender:
 			if (hasFocus) {
 				signupCallback.openGenderInputDialog();
@@ -177,11 +195,21 @@ implements OnClickListener, OnFocusChangeListener{
 		uploadImage.setImageDrawable(d);
 	}
 
-	public void setProfileImage(Bitmap fileBitmap, byte[] fileData, String filename) {
+	public void setProfileImage(Bitmap fileBitmap, byte[] fileData,
+			String filename) {
 		uploadImage.setImageBitmap(fileBitmap);
 		uploadImage.setBackgroundColor(getResources().getColor(
 				android.R.color.transparent));
 		profilePictureData = Arrays.copyOf(fileData, fileData.length);
 		profilePictureName = filename;
+	}
+
+	public void turnOnProgress() {
+		signupProgress.setVisibility(View.VISIBLE);
+
+	}
+
+	public void turnOffProgress() {
+		signupProgress.setVisibility(View.INVISIBLE);
 	}
 }
